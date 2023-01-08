fa15f960099791c7ed73bb7ec5cc88faf2a62457
HildibjørgDidriksen <s164539@student.dtu.dk>
2023-01-08 17:04:51 +0100

    I tried grouping the scenarios with comments. Some scenarios are re-uses of other scenarios, so if you can't find them grouped then that is probably why.

--- demo_client/src/test/java/dtuPayService/SimpleDTUPaySteps.java
+++ demo_client/src/test/java/dtuPayService/SimpleDTUPaySteps.java
@@ -9,15 +9,15 @@ import io.cucumber.java.en.Given;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.ArrayList;
 
-import jakarta.ws.rs.core.MediaType;
 import org.acme.PaymentLogEntry;
 import org.acme.ResponseStatus;
 
 import static org.junit.Assert.*;
 
 public class SimpleDTUPaySteps {
+
+    //Test setup:
     User customer = new User();
     User merchant = new User();
     String cid, mid, mAccount, cAccount;
@@ -27,60 +27,7 @@ public class SimpleDTUPaySteps {
     ResponseStatus responseStatus;
     PaymentLogEntry[] paymentLogEntryList;
 
-    @Given("a customer with id {string}")
-    public void aCustomerWithId(String cid) {
-        this.cid = cid;
-    }
-    @Given("a merchant with id {string}")
-    public void aMerchantWithId(String mid) {
-        this.mid = mid;
-    }
-    @When("the merchant initiates a payment for {int} kr by the customer")
-    public void theMerchantInitiatesAPaymentForKrByTheCustomer(int amount) {
-        responseStatus = dtuPay.pay(amount,cid,mid);
-    }
-    @Then("the payment is successful")
-    public void thePaymentIsSuccessful() {
-        assertTrue(responseStatus.status);
-    }
-
-    @Given("a successful payment of {string} kr from customer {string} to merchant {string}")
-    public void aSuccessfulPaymentOfKrFromCustomerToMerchant(String amount, String cid, String mid) {
-        this.cid = dtuPay.register(cid, cid, "cbank");
-        this.mid = dtuPay.register(mid, mid, "mbank");
-        this.amount = Integer.parseInt(amount);
-        responseStatus = dtuPay.pay(this.amount,this.cid,this.mid);
-    }
-
-    @When("the manager asks for a list of payments")
-    public void theManagerAsksForAListOfPayments() {
-        paymentLogEntryList = dtuPay.getPaymentLog();
-
-    }
-
-    @Then("the list contains a payment where the customer paid {string} kr to the merchant")
-    public void theListContainsAPaymentWhereCustomerPaidKrToMerchant(String amount) {
-        int newAmount = Integer.parseInt(amount);
-        PaymentLogEntry goal = new PaymentLogEntry(newAmount, cid, mid);
-        assertTrue(Arrays.asList(paymentLogEntryList).contains(goal));
-    }
-
-    @When("the merchant initiates a payment for {string} kr by the customer")
-    public void theMerchantInitiatesAPaymentForKrByTheCustomer(String amount) {
-        int newAmount = Integer.parseInt(amount);
-        responseStatus = dtuPay.pay(newAmount,this.cid,this.mid);
-    }
-
-    @Then("the payment is not successful")
-    public void thePaymentIsNotSuccessful() {
-        assertFalse(responseStatus.status);
-    }
-
-    @And("an error message is returned saying {string}")
-    public void anErrorMessageIsReturnedSaying(String errorMessage) {
-        assertEquals(errorMessage, responseStatus.errorMessage);
-    }
-
+    //Scenario Successful Payment:
     @Given("a customer with a bank account with balance {int}")
     public void aCustomerWithABankAccountWithBalance(int arg0) {
         customer.setFirstName("Johnny");
@@ -116,6 +63,60 @@ public class SimpleDTUPaySteps {
         mid = dtuPay.register(merchant.getFirstName()+" "+merchant.getLastName(), merchant.getCprNumber(), mAccount);
     }
 
+    @When("the merchant initiates a payment for {int} kr by the customer")
+    public void theMerchantInitiatesAPaymentForKrByTheCustomer(int amount) {
+        responseStatus = dtuPay.pay(amount,cid,mid);
+    }
+
+    @Then("the payment is successful")
+    public void thePaymentIsSuccessful() {
+        assertTrue(responseStatus.status);
+    }
+
+    //List of payments
+    @When("the manager asks for a list of payments")
+    public void theManagerAsksForAListOfPayments() {
+        paymentLogEntryList = dtuPay.getPaymentLog();
+
+    }
+
+    @Then("the list contains a payment where the customer paid {string} kr to the merchant")
+    public void theListContainsAPaymentWhereCustomerPaidKrToMerchant(String amount) {
+        int newAmount = Integer.parseInt(amount);
+        PaymentLogEntry goal = new PaymentLogEntry(newAmount, cid, mid);
+        assertTrue(Arrays.asList(paymentLogEntryList).contains(goal));
+    }
+
+    //The customer is not known
+    @Given("a customer with id {string}")
+    public void aCustomerWithId(String cid) {
+        this.cid = cid;
+    }
+
+    @Then("the payment is not successful")
+    public void thePaymentIsNotSuccessful() {
+        assertFalse(responseStatus.status);
+    }
+
+    @And("an error message is returned saying {string}")
+    public void anErrorMessageIsReturnedSaying(String errorMessage) {
+        assertEquals(errorMessage, responseStatus.errorMessage);
+    }
+
+    //The merchant is not known
+    @Given("a merchant with id {string}")
+    public void aMerchantWithId(String mid) {
+        this.mid = mid;
+    }
+
+
+    //Successful payment 2:
+    @When("the merchant initiates a payment for {string} kr by the customer")
+    public void theMerchantInitiatesAPaymentForKrByTheCustomer(String amount) {
+        int newAmount = Integer.parseInt(amount);
+        responseStatus = dtuPay.pay(newAmount,this.cid,this.mid);
+    }
+
     @And("the balance of the customer at the bank is {int} kr")
     public void theBalanceOfTheCustomerAtTheBankIsKr(int arg0) {
         try {
@@ -134,6 +135,8 @@ public class SimpleDTUPaySteps {
         }
     }
 
+
+//cleanup:
     @After()
     public void Cleanup() {
         if(cAccount != null){
@@ -154,5 +157,14 @@ public class SimpleDTUPaySteps {
                 fail("Failed cleanup ):");
             }
         }
+    }
+
+    // is not used by any scenarios
+    @Given("a successful payment of {string} kr from customer {string} to merchant {string}")
+    public void aSuccessfulPaymentOfKrFromCustomerToMerchant(String amount, String cid, String mid) {
+        this.cid = dtuPay.register(cid, cid, "cbank");
+        this.mid = dtuPay.register(mid, mid, "mbank");
+        this.amount = Integer.parseInt(amount);
+        responseStatus = dtuPay.pay(this.amount,this.cid,this.mid);
     }
 }
