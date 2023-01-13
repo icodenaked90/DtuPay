2376e058d64c39fca9f63587128c48592ce21cca
Adin1128 <Adin11281128@hotmail.com>
2023-01-13 14:35:56 +0100

    fixed manager

--- AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
+++ AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
@@ -9,7 +9,7 @@ import messaging.Event;
 import messaging.MessageQueue;
 import java.util.UUID;
 
-public class AccountHandler {
+public class AccountIdService {
 
     public static final String ACCOUNT_REGISTRATION_REQUESTED = "AccountRegistrationRequested";
     public static final String ACCOUNT_ID_ASSIGNED = "AccountIdAssigned";
@@ -17,7 +17,7 @@ public class AccountHandler {
     private HashMap<String, String> userAccounts = new HashMap<String, String>();
     private Map<CorrelationId, CompletableFuture<Account>> correlations = new ConcurrentHashMap<>();
 
-    public AccountHandler(MessageQueue q) {
+    public AccountIdService(MessageQueue q) {
         queue = q;
         queue.addHandler(ACCOUNT_REGISTRATION_REQUESTED, this::handleAccountRegistrationRequested);
     }
@@ -31,8 +31,9 @@ public class AccountHandler {
     public void handleAccountRegistrationRequested(Event e) {
         var a = e.getArgument(0, Account.class);
         var correlationid = e.getArgument(1, CorrelationId.class);
-        //TODO:
-        correlations.get(correlationid).complete(a);
+
+        Event event = new Event(ACCOUNT_ID_ASSIGNED, new Object[] { register(a), correlationid });
+        queue.publish(event);
     }
 
     private String generateId(){
