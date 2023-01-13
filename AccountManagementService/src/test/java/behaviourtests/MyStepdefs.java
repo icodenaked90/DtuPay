4755883069523ab5f73d94fc459e1c9208c44d30
Hildibjørg <s164539@student.dtu.dk>
2023-01-13 11:57:17 +0100

    Created step def for cucumber tests

---
+++ AccountManagementService/src/test/java/behaviourtests/MyStepdefs.java
@@ -0,0 +1,28 @@
+package behaviourtests;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("a user with an empty account id")
+    public void aUserWithAnEmptyAccountId() {
+    }
+
+    @When("a {string} event is received")
+    public void aEventIsReceived(String arg0) {
+
+    }
+
+    @Then("a {string} event is sent")
+    public void aEventIsSent(String arg0) {
+
+    }
+
+    @And("the customer receives an account id")
+    public void theCustomerReceivesAnAccountId() {
+        
+    }
+
+}
