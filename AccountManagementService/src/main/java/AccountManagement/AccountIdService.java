eb85ffca43c88d012bcf5176f382fbf84d325862
baldm <jonathan@zoern.dk>
2023-01-13 15:25:50 +0100

    Fixed account service

--- AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
+++ AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
@@ -9,13 +9,9 @@ import messaging.Event;
 import messaging.MessageQueue;
 import java.util.UUID;
 
-<<<<<<<< HEAD:AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
 //Author: Adin s164432
 //Author: Jonathan s194134
 public class AccountIdService {
-========
-public class AccountService {
->>>>>>>> origin/AM_cucumberTest:AccountManagementService/src/main/java/AccountManagement/AccountService.java
 
     public static final String ACCOUNT_REGISTRATION_REQUESTED = "AccountRegistrationRequested";
     public static final String ACCOUNT_ID_ASSIGNED = "AccountIdAssigned";
@@ -23,11 +19,8 @@ public class AccountService {
     private HashMap<String, String> userAccounts = new HashMap<String, String>();
     private Map<CorrelationId, CompletableFuture<Account>> correlations = new ConcurrentHashMap<>();
 
-<<<<<<<< HEAD:AccountManagementService/src/main/java/AccountManagement/AccountIdService.java
+
     public AccountIdService(MessageQueue q) {
-========
-    public AccountService(MessageQueue q) {
->>>>>>>> origin/AM_cucumberTest:AccountManagementService/src/main/java/AccountManagement/AccountService.java
         queue = q;
         queue.addHandler(ACCOUNT_REGISTRATION_REQUESTED, this::handleAccountRegistrationRequested);
     }
