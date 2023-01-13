c0ee66e9def801f8eec654dfb4063bda0a2c20ac
baldm <jonathan@zoern.dk>
2023-01-13 17:14:49 +0100

    it works now

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -3,5 +3,5 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitMq
 sleep 10
-docker-compose up -d facade-service accountmanagment-service
+docker-compose up -d facade-service account-management-service
 
