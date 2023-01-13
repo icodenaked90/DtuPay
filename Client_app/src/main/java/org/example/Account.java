2fb73ba627be6e7f317941552abe522d728d6247
icodenaked90 <43744105+icodenaked90@users.noreply.github.com>
2023-01-13 11:45:43 +0100

    start commit

---
+++ Client_app/src/main/java/org/example/Account.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Account {
+    String name;
+    String CPR;
+    String bankAccount;
+}
---
+++ Client_app/src/main/java/org/example/MerchantAppService.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import dtu.ws.fastmoney.AccountInfo;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import org.acme.PaymentLogEntry;
+import org.acme.ResponseStatus;
+import org.acme.SimpleDTUPayAccount;
+
+public class MerchantAppService {
+    WebTarget baseUrl;
+
+    public MerchantAppService() {
+        Client client = ClientBuilder.newClient();
+        // baseUrl = client.target("http://host.docker.internal:8080/");
+        baseUrl = client.target("http://localhost:8080/");
+    }
+
+    private PaymentLogEntry[] getLog(String mediaType) {
+
+        return baseUrl.path("merchant/report")
+                .request()
+                .accept(mediaType)
+                .get(PaymentLogEntry[].class);
+    }
+
+    private ResponseStatus setNewPayment(PaymentLogEntry payment, String mediaType) {
+        var response = baseUrl.path("merchant/payment")
+                .request()
+                .post(Entity.entity(payment, mediaType));
+
+        if (response.getStatus() == 200) {
+            return new ResponseStatus(true, response.readEntity(String.class));
+        }
+
+        return new ResponseStatus(false, response.readEntity(String.class));
+    }
+
+    private String registerNewMerchantAccount(Account account) {
+
+        var response = baseUrl.path("merchant/account")
+                .request()
+                .post(Entity.entity(account, MediaType.APPLICATION_JSON));
+
+        if (response.getStatus() == 200) {
+            String output = response.readEntity(String.class);
+            return output;
+        }
+
+        return "fail";
+    }
+}
