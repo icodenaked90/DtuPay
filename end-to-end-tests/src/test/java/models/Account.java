11a9c562956cfcc6e03be65cbec4037ad7719b23
icodenaked90 <43744105+icodenaked90@users.noreply.github.com>
2023-01-13 12:57:58 +0100

    Create test merchant draft

---
+++ end-to-end-tests/src/test/java/models/Account.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Account {
+    private String name;
+    private String cpr;
+    private String bankAccount;
+
+    public Account(String _name, String _cpr, String _bankAccount){
+        name = _name;
+        cpr = _cpr;
+        bankAccount = _bankAccount;
+    }
+}
--- end-to-end-tests/src/test/resources/MerchantRegistrationSteps.java
+++ end-to-end-tests/src/test/resources/MerchantRegistrationSteps.java
@@ -2,10 +2,17 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import models.Account;
 
 public class MerchantRegistrationSteps {
+    Account merchant;
+
+
+
+
     @Given("an unregistered merchant with empty id")
     public void anUnregisteredMerchantWithEmptyId() {
+        merchant = new Account("John", "123456-1234", "")
     }
 
     @When("the merchant is being registered")
