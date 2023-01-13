9859a1175bdbc250f4c12cb8cabd8158d70fd4b7
icodenaked90 <43744105+icodenaked90@users.noreply.github.com>
2023-01-13 10:08:43 +0100

    cleanup

---
+++ Dtupay/src/main/java/org/acme/DTUPayService.java
@@ -0,0 +1,23 @@
+package org.acme;
+//Author: Adin s164432
+public class DTUPayService {
+    private MessageQueue queue;
+
+    public DTUPayService(MessageQueue q) {
+        queue = q;
+        queue.addHandler("registrationIDAssigned", this::handleStudentIdAssigned);
+
+    }
+
+    public Account register(Account a) {
+        registeredStudent = new CompletableFuture<>();
+        Event event = new Event("StudentRegistrationRequested", new Object[] { a });
+        queue.publish(event);
+        return registeredStudent.join();
+    }
+
+    public void handleStudentIdAssigned(Event e) {
+        var s = e.getArgument(0, Student.class);
+        registeredStudent.complete(s);
+    }
+}
--- student-registration-service/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-mvn clean package
-docker-compose build student-registration-service 
