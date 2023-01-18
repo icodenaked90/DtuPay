ed952003c2fcea160aed33c84e892846697d6ba8
Hildibjørg <s164539@student.dtu.dk>
2023-01-18 12:16:38 +0100

    Updated the response to handle error messages

--- Facade/src/main/java/org/acme/MerchantResource.java
+++ Facade/src/main/java/org/acme/MerchantResource.java
@@ -1,7 +1,7 @@
 /*
 @Author: Mila s223313
 @Author: Adin s164432
-...
+@Author: Hildibjørg s164539
  */
 
 package org.acme;
@@ -42,10 +42,22 @@ public class MerchantResource {
     public Response registerAccount(Account account) {
 
         //TODO: Delete the account using service
-        String mid = dtuPay.register(account);
-        // Everything went well
-        return Response.ok(mid).build(); //TODO: write correct response
+
+        String response = dtuPay.register(account);
+
+        //check name
+        if (response.equals("Name has a wrong format")) {
+            return Response.status(400).entity(response).build();
+        }
+        //check cpr
+        else if (response.equals("CPR number has a wrong format")){
+            return Response.status(400).entity(response).build();
+        }
+        //everything went well
+        else {return Response.ok(response).build(); //TODO: Send correct message
+        }
     }
+
     @DELETE
     @Path("/account/{id}")
     @Produces(MediaType.TEXT_PLAIN)
