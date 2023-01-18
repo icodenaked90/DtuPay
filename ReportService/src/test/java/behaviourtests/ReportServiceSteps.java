ae37126d78aabca61d670b58b1b390859bdd8166
Adin1128 <Adin11281128@hotmail.com>
2023-01-18 07:16:43 +0100

    added file

--- ReportService/src/test/java/behaviourtests/ReportServiceSteps.java
+++ ReportService/src/test/java/behaviourtests/ReportServiceSteps.java
@@ -1,2 +1,39 @@
 public class ReportServiceSteps {
+    @Given("User has an ID")
+    public void userHasAnID() {
+        userId = "aa";
+    }
+
+    @And("A user requests a report")
+    public void aUserRequestsAReport() {
+        corId = CorrelationId.randomId();
+        e = new Event(REPORT_REQUESTED, new Object[] {userId , corId});
+        service.queue.publish(e);
+    }
+
+
+
+    @When("a {string} event is received")
+    public void aEventIsReceived(String arg0) {
+        e = new Event(arg0, new Object[] { userId, corId });
+        System.out.println("See me");
+        System.out.println(arg0 + "   " + userId + "   " + corId);
+        verify(service.queue).publish(e);
+
+    }
+
+
+    @Then("the event is handled")
+    public void theEventIsHandled() {
+        service.handleReportRequested(e);
+    }
+
+    @Then("a {string} event is sent")
+    public void aEventIsSent(String arg0) {
+        e = new Event(arg0, new Object[] { userId, corId });
+        verify(service.queue).publish(e);
+    }
+
+
+
 }
