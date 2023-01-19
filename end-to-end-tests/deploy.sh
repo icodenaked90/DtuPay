81d5b8f4e5ce3b0e5d8811239145eb2387c851be
baldm <jonathan@zoern.dk>
2023-01-19 14:33:01 +0100

    build script for report

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -3,4 +3,4 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitMq
 sleep 10
-docker-compose up -d facade-service account-management-service payment-service token-management-service
+docker-compose up -d facade-service account-management-service payment-service token-management-service report-service
