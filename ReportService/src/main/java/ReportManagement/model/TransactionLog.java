5dacfb9bb80be8d80d788b578911721c3e85a8fe
Icodenaked90 <simonphilipsen1990@gmail.com>
2023-01-19 14:04:54 +0100

    copy transaction log

--- ReportService/src/main/java/ReportManagement/model/TransactionLog.java
+++ ReportService/src/main/java/ReportManagement/model/TransactionLog.java
@@ -3,9 +3,16 @@ package ReportManagement.model;
 import java.util.ArrayList;
 
 public class TransactionLog {
+    String requesterId;
     ArrayList<Transaction> log = new ArrayList<>();
 
     public void addToLog(Transaction transaction) {
         log.add(transaction);
+    }
+    public String getId() {
+        return  requesterId;
+    }
+    public void setId(String id) {
+        this.requesterId = id;
     }
 }
